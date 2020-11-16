29af8fd73eccd2e0ae38442347fe90c6dbf7d2d0
unknown <wolodjawaskovych@i.ua>
2020-11-16 13:12:01 +0100

    Expansion of Customer API

--- src/main/java/guru/springframework/msbrewery/web/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msbrewery/web/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msbrewery.web.services;
 
 import guru.springframework.msbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
@@ -14,5 +16,23 @@ public class CustomerServiceImpl implements CustomerService {
             .id(UUID.randomUUID())
             .name("John Smith")
             .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return  CustomerDto.builder()
+            .id(UUID.randomUUID())
+            .name("John Smith")
+            .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        log.debug("Updating customer...");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting customer...");
     }
 }
