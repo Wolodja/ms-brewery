49178df3600d7b55f2426c81fe7857c804f02473
unknown <wolodjawaskovych@i.ua>
2020-11-28 17:52:07 +0100

    Using Lombok

--- src/main/java/guru/springframework/msbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msbrewery/web/controller/CustomerController.java
@@ -2,26 +2,24 @@ package guru.springframework.msbrewery.web.controller;
 
 import guru.springframework.msbrewery.web.model.CustomerDto;
 import guru.springframework.msbrewery.web.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/customer/")
 @RestController
 public class CustomerController {
 
     private final CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
 
     @GetMapping("/{customerId}")
     ResponseEntity<CustomerDto> getCustomerById(@PathVariable UUID customerId) {
@@ -31,9 +29,9 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity handlePost(@RequestBody @Valid CustomerDto customerDto) {
 
-        CustomerDto savedCustomerDto = customerService.saveNewCustomer(customerDto);
+        val savedCustomerDto = customerService.saveNewCustomer(customerDto);
 
-        HttpHeaders headers = new HttpHeaders();
+        val headers = new HttpHeaders();
         headers.add("Location", "/api/v1/customer/" + savedCustomerDto.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
