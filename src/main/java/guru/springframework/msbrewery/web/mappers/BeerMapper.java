9fde09eb9f444394000133f2670977c0da246ba9
unknown <wolodjawaskovych@i.ua>
2020-11-28 18:40:50 +0100

    Date Conversion With MapStruct

--- src/main/java/guru/springframework/msbrewery/web/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/msbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import guru.springframework.msbrewery.domain.Beer;
 import guru.springframework.msbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
